c41f2179b3fe4191e7a4dc2f5c83877b33723a18
Sumukh Prasad <sumukhprasad@icloud.com>
2023-05-15 13:52:17 +0530

    Linted code

--- test/controllers/print_shop_controller_test.rb
+++ test/controllers/print_shop_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class PrintShopControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get print_shop_index_url
-    assert_response :success
-  end
+	test "should get index" do
+		get print_shop_index_url
+		assert_response :success
+	end
 end
